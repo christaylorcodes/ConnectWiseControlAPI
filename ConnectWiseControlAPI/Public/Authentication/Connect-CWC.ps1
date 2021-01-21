c6ff43c8033eaaadb2bf4c05a082ae0426f55b10
cnusj-admin <68920438+cnusj-admin@users.noreply.github.com>
2021-01-20 19:53:10 -0500

    Update Connect-CWC.ps1

--- ConnectWiseControlAPI/Public/Authentication/Connect-CWC.ps1
+++ ConnectWiseControlAPI/Public/Authentication/Connect-CWC.ps1
@@ -1,10 +1,10 @@
 function Connect-CWC {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory=$true)][string] $Server,
+        [Parameter(Mandatory = $true)][string] $Server,
         [Parameter(Mandatory = $True)][pscredential] $Credentials,
-        [switch]$Force,
-        [string] $aspxauth
+        [switch] $Force,
+        [string] $Aspxauth,
         [string] $CloudInstanceAuth
     )
 
@@ -13,44 +13,50 @@ function Connect-CWC {
         return
     }
 
-    $Server = $Server -replace("http.*:\/\/",'')
+    $Server = $Server -replace ("http.*:\/\/", '')
     
-    if($credentials -ne "CookieAuth") {
-    $encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($Credentials.UserName):$($Credentials.GetNetworkCredential().Password)"))
-    $Headers = @{
-        'authorization' = "Basic $encodedCredentials"
-        'content-type' = "application/json; charset=utf-8"
-        'origin' = "https://$Server"
+    if ($credentials -ne "CookieAuth") {
+        $encodedCredentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($Credentials.UserName):$($Credentials.GetNetworkCredential().Password)"))
+        $Headers = @{
+            'authorization' = "Basic $encodedCredentials"
+            'content-type'  = "application/json; charset=utf-8"
+            'origin'        = "https://$Server"
+        }
     }
-    }else{
-    $cookieAuth=$true
-    $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession    
-    $cookie = New-Object System.Net.Cookie 
+    else {
+        if((!$Aspxauth) -or (!$CloudInstanceAuth)) {
+            Write-host "Cookie Auth was specified, but one or more of Aspxauth or CloudInstanceAuth"
+        }
+        $cookieAuth = $true
+        $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession    
+        $cookie = New-Object System.Net.Cookie 
 
         $cookie.Name = ".ASPXAUTH"
         $cookie.Value = ""
         $cookie.Domain = "$server"
-    $session.Cookies.Add($cookie);
+        $session.Cookies.Add($cookie);
+
         $cookie.Name = "CloudInstanceAuth"
         $cookie.Value = "F20RqixToMuhK9aW94FNfbjB95Bc5zCI+WpynWWANPpoAAAGcXVjYmpsG0Nsb3VkIEFjY291bnQgQWRtaW5pc3RyYXRvcgIAAACW4meSQJlw+engri1CUmfFR6OyLUIAAQZxdWNiamxOR8X6L68tQgAB"
         $cookie.Domain = "$server"
-    $session.Cookies.Add($cookie);
+        $session.Cookies.Add($cookie);
+
         $cookie.Name = "Settings"
         $cookie.Value = "%7B%22extendedCss%22%3A%7B%22grid-resizable-column-edges%22%3A%7B%7D%7D%2C%22selectedTabBySessionTypeMap%22%3A%7B%222%22%3A%22General%22%7D%2C%22collapsedPanelMap%22%3A%7B%22Screenshot%22%3Atrue%7D%7D"
         $cookie.Domain = "$server"
-    $session.Cookies.Add($cookie);
+        $session.Cookies.Add($cookie);
     
-     $Headers = @{        
-        'content-type' = "application/json; charset=utf-8"
-        'origin' = "https://$Server"
-    }
+        $Headers = @{        
+            'content-type' = "application/json; charset=utf-8"
+            'origin'       = "https://$Server"
+        }
     
     }
     
 
     $script:CWCServerConnection = @{
         Session = $Session        
-        Server = $Server
+        Server  = $Server
         Headers = $Headers
     }
 
